90e9a2310be202f918d29d159557c8796e3ae124
Albina Starykova <albina.starikova@gmail.com>
2022-09-01 14:54:39 +0200

    Add css & assets management for webpack

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './style.css';
+
+/* 
+
+todo app
+
+dom
+- inbox (all)
+- today 
+- this week
+- completed
+- projects list
+
+[projects]
+
+{project}
+- title
+- data-key for dom remove/edit
+- [todo items]
+
+{todo item}:
+- title
+- desc
+- dueDate
+- isComplete
+- data-key for dom remove/edit
+
+app logic object or module?
+- creates todos
+- setting todos as complete
+- changing priority, date, desc etc
+
+DOMdtuff
+- visualise app logic
+
+
+ref https://artis-dev.github.io/to-do-list/#
+ */
